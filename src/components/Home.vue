ae99ef6a14073bb17a58f042508d1edb5fd0a254
Derri <derrisahputra12>
2020-02-16 23:08:04 +0700

    berubah

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,6 +4,7 @@
         <h2>Aku udah ada buat perubahan loh</h2>
         <h3>Dan aku buat perubahan lagi nih</h3>
         <h4>Lagi lagi</h4>
+        <h5>Dan ini berubahnya dari local repo</h5>
     </div>
 </template>
 
