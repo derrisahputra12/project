a828ab6c8d9f108ad60941ee168e3a79d436a286
Derri <derrisahputra12>
2020-02-16 22:47:48 +0700

    second commit

---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div id="about">
+        <h1>Ini adalah about</h1>
+    </div>
+</template>
+``
---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="home">
+        <h1>Ini adalah Home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    el: "#home"
+}
+</script>
